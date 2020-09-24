7a6c18e1f6a47e9e815f4fc6c8d3126c97f76577
jacheung <jacheung6@gmail.com>
2020-09-23 17:34:37 -0700

    updated custom generator to allow inputs for training and prediction

--- autocurator/data_prepare/load_images.py
+++ autocurator/data_prepare/load_images.py
@@ -4,31 +4,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 
-def build_data(x_files, y_files):
-
-  to_del = 0
-  cnt1 = -1
-
-  for k in range(len(y_files)):
-    cnt1 = cnt1 + 1
-    tmp1 = scipy.io.loadmat(x_files[cnt1])
-    tmp2 = scipy.io.loadmat(y_files[cnt1])
-
-    Xtmp = tmp1['finalMat']
-    Ytmp = tmp2['touchClass']
-    if cnt1==0:
-      raw_X = Xtmp
-      raw_Y = Ytmp
-    else:
-
-      raw_X = np.concatenate((raw_X,Xtmp), axis=0)
-      raw_Y = np.concatenate((raw_Y,Ytmp), axis=0)
-
-  return raw_X, raw_Y
 
 class My_Custom_Generator(keras.utils.Sequence):
-"""Keras class that uses that chunks the names and labels for batch processing"""
-    def __init__(self, file_trial_list, file_Y_list, num_in_each, batch_size):
+
+    def __init__(self, file_trial_list, file_Y_list, num_in_each, batch_size, to_fit):
         cnt = 0
         extract_inds = []
         # num_in_each contains the number of frames in each file I am loading, ie
@@ -52,6 +31,7 @@ class My_Custom_Generator(keras.utils.Sequence):
             file_list_chunks.append(file_trial_list[ii[0]:ii[1]])
             file_Y_list_chunks.append(file_Y_list[ii[0]:ii[1]])
 
+        self.to_fit = to_fit  # set to True to return XY and False to return X
         self.file_trial_list = file_trial_list
         self.file_Y_list = file_Y_list
         self.batch_size = batch_size
@@ -64,11 +44,13 @@ class My_Custom_Generator(keras.utils.Sequence):
         return len(self.extract_inds)
 
     def __getitem__(self, num_2_extract):
-        raw_X, raw_Y = build_data(self.file_list_chunks[num_2_extract],
-                                  self.file_Y_list_chunks[num_2_extract])
+        # raw_X, raw_Y = self._build_data(self.file_list_chunks[num_2_extract],
+        #                       self.file_Y_list_chunks[num_2_extract])
+
+        raw_X = self._generate_X(self.file_list_chunks[num_2_extract])
 
         rgb_batch = np.repeat(raw_X[..., np.newaxis], 3, -1)
-        IMG_SIZE = 96  # All images will be resized to 96x96. This is the size of MobileNetV2 input sizes
+        IMG_SIZE = 96  # All images will be resized to 160x160. This is the size of MobileNetV2 input sizes
 
         rgb_tensor = tf.cast(rgb_batch, tf.float32)  # convert to tf tensor with float32 dtypes
         rgb_tensor = (rgb_tensor / 127.5) - 1  # /127.5 = 0:2, -1 = -1:1 requirement for mobilenetV2
@@ -76,11 +58,27 @@ class My_Custom_Generator(keras.utils.Sequence):
 
         self.IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
         rgb_tensor_aug = rgb_tensor
-        return rgb_tensor_aug, raw_Y
+
+        # print(len(raw_Y))
+        # for i, ims in enumerate(rgb_tensor):
+        #   # print(i)
+        #   tmp1 = fux_wit_imgs(20, ims)
+        #   rgb_tensor_aug = np.append(rgb_tensor_aug, tmp1, 0)
+
+        if self.to_fit:
+            raw_Y = self._generate_Y(self.file_Y_list_chunks[num_2_extract])
+            return rgb_tensor_aug, raw_Y
+        else:
+            return rgb_tensor_aug
+
+    # def _getitem__tmp(self, touch_aug_num, no_touch_aug_num)
 
     def get_single_trials(self, num_2_extract):
-        raw_X, raw_Y = build_data([self.file_trial_list[num_2_extract]],
-                                  [self.file_Y_list[num_2_extract]])
+        # raw_X, raw_Y = self._build_data([self.file_trial_list[num_2_extract]],
+        #                       [self.file_Y_list[num_2_extract]])
+
+        raw_X = self._generate_X(self.file_list_chunks[num_2_extract])
+        raw_Y = self._generate_Y(self.file_Y_list_chunks[num_2_extract])
 
         frame_index = scipy.io.loadmat(self.frame_ind_list[num_2_extract])
         frame_index = frame_index['relevantIdx']
@@ -94,11 +92,67 @@ class My_Custom_Generator(keras.utils.Sequence):
 
         self.IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
         rgb_tensor_aug = rgb_tensor
-
+        # print(len(raw_Y))
+        # for i, ims in enumerate(rgb_tensor):
+        #   print(i)
+        #   tmp1 = fux_wit_imgs(20, ims)
+        #   rgb_tensor_aug = np.append(rgb_tensor_aug, tmp1, 0)
         return rgb_tensor_aug, raw_Y
 
         # return rgb_tensor, raw_Y, frame_index#, trial_file_num
 
+    # Function to generate an image tensor and corresponding label array
+
+    def _build_data(self, x_files, y_files):
+        """Phils original build data structure used to generate X and Y together. It has been broken down into _generate_X and _generate_Y. Delete ASAP"""
+        cnt1 = -1;
+
+        for k in range(len(y_files)):
+            cnt1 = cnt1 + 1
+            tmp1 = scipy.io.loadmat(x_files[cnt1])
+            tmp2 = scipy.io.loadmat(y_files[cnt1])
+
+            Xtmp = tmp1['finalMat']
+            Ytmp = tmp2['touchClass']
+            if cnt1 == 0:
+                raw_X = Xtmp
+                raw_Y = Ytmp
+            else:
+
+                raw_X = np.concatenate((raw_X, Xtmp), axis=0)
+                raw_Y = np.concatenate((raw_Y, Ytmp), axis=0)
+
+        return raw_X, raw_Y
+
+    def _generate_X(self, x_files):
+        cnt1 = -1;
+
+        for k in range(len(x_files)):
+            cnt1 = cnt1 + 1
+            tmp1 = scipy.io.loadmat(x_files[cnt1])
+            Xtmp = tmp1['finalMat']
+
+            if cnt1 == 0:
+                raw_X = Xtmp
+            else:
+                raw_X = np.concatenate((raw_X, Xtmp), axis=0)
+
+        return raw_X
+
+    def _generate_Y(self, y_files):
+        cnt1 = -1;
+
+        for k in range(len(y_files)):
+            cnt1 = cnt1 + 1
+            tmp2 = scipy.io.loadmat(y_files[cnt1])
+
+            Ytmp = tmp2['touchClass']
+            if cnt1 == 0:
+                raw_Y = Ytmp
+            else:
+                raw_Y = np.concatenate((raw_Y, Ytmp), axis=0)
+
+        return raw_Y
 
     def plot_batch_distribution(self):
         # randomly select a batch and generate images and labels
