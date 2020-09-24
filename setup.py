e0ead0fbab25f82c16ea54ce11fe889e6558c2f8
jacheung <jacheung6@gmail.com>
2020-09-24 15:09:24 -0700

    reorganization and refactoring set up

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A pre-trained CNN for touch classification of whisker to objects",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jacheung/auto-curator",
+    url="https://github.com/jacheung/autocurator",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
